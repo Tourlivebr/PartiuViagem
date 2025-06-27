ecf50ea1cc9834835b3461710ed41d32565087f1
tourlive <tourlivebr@gmail.com>
2025-06-27 17:11:29 -0300

    ago

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Partiu Viagem';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Viva momentos inesquecíveis!';
