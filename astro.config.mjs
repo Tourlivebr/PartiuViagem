b76f562ae3386a61919e708613c68c03dde60938
tourlive <tourlivebr@gmail.com>
2025-06-30 20:04:24 -0300

    atualizaagora

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://partiu-viagem-three.vercel.app',
   integrations: [mdx(), sitemap(), tailwind()],
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  })
+  output: 'static'
 });
